dd2d6ad1876bed7a01ca07e7c8424df3e40f3a5d
Maarten de Ruyter <devops@geodatagarden.com>
2025-12-12 15:16:06 +0100

    DGDM-32 Added age gender group model to death statistics

--- src/datagarden_models/models/health/death_statistics.py
+++ src/datagarden_models/models/health/death_statistics.py
@@ -1,6 +1,7 @@
 from pydantic import Field
 
 from datagarden_models.models.base import DataGardenSubModel
+from datagarden_models.models.demographics.base_demographics import AgeGenderStatistics
 
 from .base_health import ByGender
 
@@ -15,9 +16,22 @@ class DeathStatisticsLegends:
 		" (for detailed description of IDC10 categories (keys in this dataset))"
 		" Death rate in deaths per 100.000 population."
 	)
+	DEATH_RATE_BY_GENDER = "Death rate by gender. In deaths per 100.000 population."
+	DEATH_RATE_BY_AGE_GENDER = (
+		"Death rate by age or age group and gender. In deaths per 100.000 population."
+	)
 
 
 L = DeathStatisticsLegends
+
+
+class DeathRateIDC10(DataGardenSubModel):
+	by_gender: ByGender = Field(
+		default_factory=ByGender, description=L.DEATH_RATE_BY_GENDER
+	)
+	by_age_gender: AgeGenderStatistics = Field(
+		default_factory=AgeGenderStatistics, description=L.DEATH_RATE_BY_AGE_GENDER
+	)
 
 
 class DeathStatistics(DataGardenSubModel):
